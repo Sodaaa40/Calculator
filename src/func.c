95016fd0c2aba8863fbc5bb41568fb5b536bbec2
Sorokovikh Darya <sorokovihd2001@icloud.com>
2020-06-04 20:14:52 +0700

    Optimise code for buttons

--- src/func.c
+++ src/func.c
@@ -12,114 +12,24 @@ extern gchar otv[20];
 extern GtkWidget* okno;
 int input(char** str, int* length);
 
-void zero_clicked(GtkWidget* widget, gpointer data)
+void button_clicked(GtkWidget* widget, gpointer data)
 {
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
+    char* dat = data;
+    if (dat[0] == '=') {
+        char* s = &text[0];
+        if (input(&s, &length))
+            return;
+        sprintf(otv, "%lf", Calc(text, 0, length - 1));
+        printf("%s\n", text);
+        memset(text, 0, 100);
+        gtk_entry_set_text(GTK_ENTRY(okno), otv);
+        length = 0;
+    } else if (dat[0] == 'C') {
+        memset(text, 0, 100);
+        gtk_entry_set_text(GTK_ENTRY(okno), text);
+    } else {
+        strcat(text, (gchar*)data);
+        gtk_entry_set_text(GTK_ENTRY(okno), text);
+    }
 }
-void one_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void two_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void three_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void four_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void five_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void six_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void seven_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void eight_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void nine_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void plus_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void minus_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void multiply_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void divide_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void pow_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void sqrt_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void leftBracket_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void rightBracket_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void point_clicked(GtkWidget* widget, gpointer data)
-{
-    strcat(text, (gchar*)data);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void delete_clicked(GtkWidget* widget, gpointer data)
-{
-    memset(text, 0, 100);
-    gtk_entry_set_text(GTK_ENTRY(okno), text);
-}
-void result_clicked(GtkWidget* widget, gpointer data)
-{
-    char* s = &text[0];
-    if (input(&s, &length))
-        return;
-    sprintf(otv, "%lf", Calc(text, 0, length - 1));
-    printf("%s\n", text);
-    memset(text, 0, 100);
-    gtk_entry_set_text(GTK_ENTRY(okno), otv);
-    length = 0;
-}
+
--- src/func.h
+++ src/func.h
@@ -2,26 +2,6 @@
 #define UI_H
 #include <gtk/gtk.h>
 
-void zero_clicked(GtkWidget* widget, gpointer data);
-void one_clicked(GtkWidget* widget, gpointer data);
-void two_clicked(GtkWidget* widget, gpointer data);
-void three_clicked(GtkWidget* widget, gpointer data);
-void four_clicked(GtkWidget* widget, gpointer data);
-void five_clicked(GtkWidget* widget, gpointer data);
-void six_clicked(GtkWidget* widget, gpointer data);
-void seven_clicked(GtkWidget* widget, gpointer data);
-void eight_clicked(GtkWidget* widget, gpointer data);
-void nine_clicked(GtkWidget* widget, gpointer data);
-void plus_clicked(GtkWidget* widget, gpointer data);
-void minus_clicked(GtkWidget* widget, gpointer data);
-void divide_clicked(GtkWidget* widget, gpointer data);
-void multiply_clicked(GtkWidget* widget, gpointer data);
-void pow_clicked(GtkWidget* widget, gpointer data);
-void sqrt_clicked(GtkWidget* widget, gpointer data);
-void delete_clicked(GtkWidget* widget, gpointer data);
-void leftBracket_clicked(GtkWidget* widget, gpointer data);
-void rightBracket_clicked(GtkWidget* widget, gpointer data);
-void point_clicked(GtkWidget* widget, gpointer data);
-void result_clicked(GtkWidget* widget, gpointer data);
+void button_clicked(GtkWidget* widget, gpointer data);
 
 #endif
